50a5e8300293a5d0ac00d0af19efb1b9d2ee8834
dgarciac2021 <92941081+dgarciac2021@users.noreply.github.com>
2022-10-27 22:24:24 +0200

    Added comments.

--- src/ledRGBe2.py
+++ src/ledRGBe2.py
@@ -1,20 +1,32 @@
 # ledRGBe2.py
 # Practica 2. Sensores y actuadores. URJC. Diego y Ioana.
 
+################################################################################
+
 import time, sys
 import RPi.GPIO as GPIO
+
+################################################################################
+
+GPIO.setmode(GPIO.BOARD)
 
 redPin = 11
 greenPin = 13
 bluePin = 15
 
-GPIO.setmode(GPIO.BOARD)
+################################################################################
 
 GPIO.setup(redPin, GPIO.OUT)
 GPIO.setup(greenPin, GPIO.OUT)
 GPIO.setup(bluePin, GPIO.OUT)
 
 colors = {"red": 4, "green": 2, "blue": 1, "cyan": 3, "magenta": 5, "yellow": 6, "white": 7, "black": 0}
+
+# Actually we are assigning numbers to each color in order to decompose them
+# in a 3 bit binary number which will represent the states of the three needed
+# outputs.
+
+################################################################################
 
 def turnOff():
    setLED("black")
@@ -25,6 +37,10 @@ def setLED(color, mode = "on"):
     rValue, gValue, bValue = bool(int(rValue)), bool(int(gValue)), bool(int(bValue))
 
     if color != "black":
+        # Only do something if yhe color to represent is not black.
+        # Actually when we turn on a color, it is substracted from the previous one.
+        # That means that only the bits that match a given color are changed.
+        # Same goes for the "on" mode.
         if mode == "off":
             if rValue: rValue = not rValue
             else: rValue = GPIO.input(redPin)
@@ -44,27 +60,36 @@ def setLED(color, mode = "on"):
     GPIO.output(redPin, rValue)
     GPIO.output(greenPin, gValue)
     GPIO.output(bluePin, bValue)
+   
+################################################################################
 
 def validInput(args):
     if len(args) == 1 and args[0] == "off" or args[0] == "exit": return True
     if len(args) == 2 and args[1] in colors and (args[0] == "on" or args[0] == "off"): return True
     print("Non valid option!")
-    return False
+    return False # Reach this point means that the argument has not passed
+    # any filter.
+
+################################################################################
 
 while True:
     while True:
         userCommand = input ("Enter your command! ->\t")
-        if not userCommand: continue
+        if not userCommand: continue # Try again if the argument is empty.
         userCommandSplitted = userCommand.lower().split(' ')
-        if validInput(userCommandSplitted): break
+        if validInput(userCommandSplitted): break # If the argument is valid...
 
     if len(userCommandSplitted) == 1:
         command = userCommandSplitted[0]
-        if command == "exit": break
+        if command == "exit": break # Break the main loop here.
         if command == "off": turnOff()
     else:
         command, color = userCommandSplitted
         if color: setLED(color, command)
-
+    
+    print("Success!")
+      
 turnOff()
 GPIO.cleanup()
+
+################################################################################
